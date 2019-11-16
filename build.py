8942f4259b0e487b3ae5c6d3cfcb3035d0deb42e
sabyassachi-mishra <sabyasachi.mishra@sy.niituniversity.in>
2019-11-16 06:04:41 +0530

    first commit

---
+++ build.py
@@ -0,0 +1,11 @@
+from pybuilder.core import init, use_plugin
+
+use_plugin("python.core")
+use_plugin("python.unittest")
+use_plugin("python.install_dependencies")
+
+default_task = "publish"
+
+@init
+def initialize(project):
+    project.build_depends_on('mockito')
---
+++ src/main/python/helloworld.py
@@ -0,0 +1,8 @@
+import sys
+
+def helloworld(out):
+    if(1):
+        out.write("Hello\n")
+    else:
+        out.write("Hello prince 1\n")
+   
